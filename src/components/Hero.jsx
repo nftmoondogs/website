37530e772db1643afb4f830309b6a36783a6b8bd
nftmoondogs <nftmoondogs@gmail.com>
2022-10-27 07:41:01 +0530

    Work6

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
     <div className="pl-0 ss:pl-6"></div>
     <a href="https://mint.woofsite.com" target="_blank" rel="noreferrer" class="hidden md:block relative max-w-sm transition-all  cursor-pointer filter grayscale hover:grayscale-0 w-[240px] items-center py-2 px-4 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-11 ml-6 w-25 h-11 mt-16">Mint</a>
 
-    <div class="w-full md:w-1/6 px-28 mb-4 ml-auto mr-auto">
+    <div class="w-full md:w-1/6 px-28 mb-4 ml-auto mr-auto ss:hidden">
   
     <div class="pt-10"></div>
 
