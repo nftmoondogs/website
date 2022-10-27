91affc765a3a77af35ea3918cd9dfac77e4a02af
nftmoondogs <nftmoondogs@gmail.com>
2022-10-27 08:47:23 +0530

    Work9

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -16,7 +16,7 @@ const CardDeal = () => (
 </div>
 
 <div className={` w-full justify-center flex w-[100%] h-[100%] pb-44`}>
-<a href="https://forms.gle/MqQRRgK5uySJx1hS7" target="_blank" rel="noreferrer"><button type="button" class="text-yellow-400 hover:text-white border justify-center flex object-center border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Enter Quest</button></a>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLScqrleKkYxYq81iZSy_VD3W3oyD4YMQjz9TmoTZiCtaiAWl_Q/viewform?usp=sf_link" target="_blank" rel="noreferrer"><button type="button" class="text-yellow-400 hover:text-white border justify-center flex object-center border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Enter Quest</button></a>
 </div>
 </section>
   )
