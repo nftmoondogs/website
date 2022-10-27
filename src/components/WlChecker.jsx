6cae8a90586174fbb0268f09e9105d5191fbe0b8
nftmoondogs <nftmoondogs@gmail.com>
2022-10-27 07:21:30 +0530

    Work3

--- src/components/WlChecker.jsx
+++ src/components/WlChecker.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
   
         <div class="pt-16"></div>
 
-        <a href="https://mint.woofsite.com" target="_blank" rel="noreferrer" class="ss:hidden ss:block justify-center mr-auto relative max-w-sm transition-all  cursor-pointer filter grayscale hover:grayscale-0 w-[240px] items-center py-2 px-4 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-11 flex mt-0">Mint</a>
+        <a href="https://mint.woofsite.com" target="_blank" rel="noreferrer" class="ss:hidden ss:block justify-center relative max-w-sm transition-all  cursor-pointer filter grayscale hover:grayscale-0 w-[240px] items-center py-2 px-4 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-11 flex mt-0">Mint</a>
       <div class="pt-2"></div>
     
       
